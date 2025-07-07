a2c0ab9b51d0c3eeed0267568f3a7b6dff2cea83
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-07-07 17:16:39 +0600

    base package added

---
+++ event-management-api/src/main/java/com/iqbal/event_management_api/EventManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.iqbal.event_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApiApplication.class, args);
+	}
+
+}
---
+++ event-management-api/src/test/java/com/iqbal/event_management_api/EventManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbal.event_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
