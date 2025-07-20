4bf966ae7e43a9045060044a2e1d13058a248ac6
Md Iqbal <mdiqbalhossain20775@gmail.com>
2025-07-20 11:44:29 +0600

    structure build up

--- event-management-ui/src/app/app.module.ts
+++ event-management-ui/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ParticipantFormComponent } from './components/participants/participant-form/participant-form.component';
+import { ParticipantListComponent } from './components/participants/participant-list/participant-list.component';
+import { EventListComponent } from './components/events/event-list/event-list.component';
+import { EventFormComponent } from './components/events/event-form/event-form.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ParticipantFormComponent,
+    ParticipantListComponent,
+    EventListComponent,
+    EventFormComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ event-management-ui/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:9090/api',
+  roles: {
+    admin: 'ADMIN',
+    organizer: 'ORGANIZER',
+    participant: 'PARTICIPANT'
+  }
+};
